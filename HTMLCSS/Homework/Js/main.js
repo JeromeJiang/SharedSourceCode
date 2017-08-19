e42a420ef1969e34bae3942ffc2e35278cbb92b8
JeromeJiang <JeromeJiang@outlook.com>
2017-08-19 13:02:13 +0800

    添加HTML css学习的作业和source code.

---
+++ HTMLCSS/Homework/Js/main.js
@@ -0,0 +1,23 @@
+﻿//实现Navigation点击后,背景颜色的变化
+function NavigationSelected(clickElement) {
+    var selectedNode = document.getElementsByClassName("navigationLinkSelected");
+    if (selectedNode.length >= 1) {
+        selectedNode[0].className = "navigationLink";
+    }
+
+    var classStr = clickElement.getAttribute("class");
+    classStr = classStr + " navigationLinkSelected";
+    clickElement.className = classStr;
+}
+
+//实现Navigation点击后,背景颜色的变化
+function TreeLinkSelected(clickElement) {
+    var selectedNode = document.getElementsByClassName("treeLinkSelected");
+    if (selectedNode.length >= 1) {
+        selectedNode[0].className = "treeLink";
+    }
+
+    var classStr = clickElement.getAttribute("class");
+    classStr = classStr + " treeLinkSelected";
+    clickElement.className = classStr;
+}
